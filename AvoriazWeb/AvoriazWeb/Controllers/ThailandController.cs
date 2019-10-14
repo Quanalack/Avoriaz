a1bbb062d856e181f278db213a004b84f6511925
Quanalack <maltehesk@hotmail.com>
2019-10-14 22:26:15 +0200

    Added thailand works

---
+++ AvoriazWeb/AvoriazWeb/Controllers/ThailandController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AvoriazWeb.Controllers
+{
+    public class ThailandController : Controller
+    {
+        // GET: Thailand
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            ViewBag.Message = "A gallery of the pictures taken in Thailand.";
+
+            return View();
+        }
+    }
+}
